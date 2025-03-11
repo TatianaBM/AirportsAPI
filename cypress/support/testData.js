d62864bb17efe78abb550c74ed68b7bbf75bc1c5
LubovLepekhina <113927670+LubovLepekhina@users.noreply.github.com>
2025-03-11 20:02:54 +0100

    divided the files and added test data (#8)

---
+++ cypress/support/testData.js
@@ -0,0 +1,16 @@
+import { faker } from '@faker-js/faker'
+
+export const dataGenerator = {
+    invalidIATACode: function() {
+        return {
+            number: faker.number.int(100),
+            'lower case': faker.airline.airport().iataCode.toLowerCase(),
+            'more than 3 characters': faker.string.alpha(4).toUpperCase(),
+            'less than 3 characters': faker.string.alpha(2).toUpperCase(),
+            'special characters': faker.string.symbol(3),
+            'mixed alphanumeric': faker.string.alphanumeric(3),
+            'null value': null,
+            'undefined value': undefined
+        }
+    }
+}
