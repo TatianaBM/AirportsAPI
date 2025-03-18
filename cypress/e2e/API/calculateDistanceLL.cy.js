d84b09e1bcb5944b87eae8ec1837185589cbc55a
LubovLepekhina <lubov.lepekhina@gmail.com>
2025-03-18 15:49:44 +0100

    add readme and add context to tests

--- cypress/e2e/API/calculateDistanceLL.cy.js
+++ cypress/e2e/API/calculateDistanceLL.cy.js
@@ -11,91 +11,94 @@ const { status_422 } = schemas.calculateDistance
 const departureId = dataGenerator.validIATACode().iataCode
 const destinationId = dataGenerator.validIATACode().iataCode
 
-describe('200 status code', () => {
+describe('calculates the distance between two airports', () => {
+    
+    context('200 status code', () => {
 
-    it('calculates the distance between two airports', () => {
-        calculateDistanceBetweenTwoAirports(endpoints.distance, departureId, destinationId)
-            .then(response => {
-                expect(response.status, 'status code').to.equal(200)
-                expect(response.body.data.id, 'id').to.equal(`${departureId}-${destinationId}`)
-                expect(response.body.data.attributes.kilometers).to.be.at.least(0)
-                expect(response.body.data.attributes.miles).to.be.at.least(0)
-                expect(response.body.data.attributes.nautical_miles).to.be.at.least(0)
-            })
+        it('calculates the distance between two airports', () => {
+            calculateDistanceBetweenTwoAirports(endpoints.distance, departureId, destinationId)
+                .then(response => {
+                    expect(response.status, 'status code').to.equal(200)
+                    expect(response.body.data.id, 'id').to.equal(`${departureId}-${destinationId}`)
+                    expect(response.body.data.attributes.kilometers).to.be.at.least(0)
+                    expect(response.body.data.attributes.miles).to.be.at.least(0)
+                    expect(response.body.data.attributes.nautical_miles).to.be.at.least(0)
+                })
+        })
+    
+        it('verifies schema for request with two valid IATA codes', () => {
+            calculateDistanceBetweenTwoAirports(endpoints.distance, departureId, destinationId)
+                .validateSchema(status_200)
+        })
     })
-
-    it('verifies schema for request with two valid IATA codes', () => {
-        calculateDistanceBetweenTwoAirports(endpoints.distance, departureId, destinationId)
-            .validateSchema(status_200)
+    
+    context('422 status code', () => {
+    
+        it(`error by sending the non-existing IATA code of the departure airport`, () => {
+            calculateDistanceBetweenTwoAirports(endpoints.distance, 'led', destinationId).should(
+                spok({
+                    status: 422,
+                    body: {
+                        "errors": [
+                            {
+                                "status": "422",
+                                "title": "Unable to process request",
+                                "detail": "Please enter valid 'from' and 'to' airports."
+                            }
+                        ]
+                    }
+                })
+            )
+        })
+    
+        it(`error by sending the non-existing IATA code of the destination airport`, () => {
+            calculateDistanceBetweenTwoAirports(endpoints.distance, departureId, 'led').should(
+                spok({
+                    status: 422,
+                    body: {
+                        "errors": [
+                            {
+                                "status": "422",
+                                "title": "Unable to process request",
+                                "detail": "Please enter valid 'from' and 'to' airports."
+                            }
+                        ]
+                    }
+                })
+            )     
+        })
+    
+        it(`error by sending the non-existing IATA code of both airports`, () => {
+            calculateDistanceBetweenTwoAirports(endpoints.distance, 'kid', 'led').should(
+                spok({
+                    status: 422,
+                    body: {
+                        "errors": [
+                            {
+                                "status": "422",
+                                "title": "Unable to process request",
+                                "detail": "Please enter valid 'from' and 'to' airports."
+                            }
+                        ]
+                    }
+                })
+            )     
+        })
+    
+        it('verifies schema for request with the non-existing IATA code of the departure airport', () => {
+            calculateDistanceBetweenTwoAirports(endpoints.distance, 'xxx', destinationId)
+                .validateSchema(status_422)
+        })
+    
+        it('verifies schema for request with the non-existing IATA code of the destination airport', () => {
+            calculateDistanceBetweenTwoAirports(endpoints.distance, departureId, 'xxx')
+                .validateSchema(status_422)
+        })
+    
+        it('verifies schema for request with the non-existing IATA code of both airports', () => {
+            calculateDistanceBetweenTwoAirports(endpoints.distance, 'xxx', 'yyy')
+                .validateSchema(status_422)
+        })
+    
     })
-})
-
-describe('422 status code', () => {
-
-    it(`error by sending the non-existing IATA code of the departure airport`, () => {
-        calculateDistanceBetweenTwoAirports(endpoints.distance, 'led', destinationId).should(
-            spok({
-                status: 422,
-                body: {
-                    "errors": [
-                        {
-                            "status": "422",
-                            "title": "Unable to process request",
-                            "detail": "Please enter valid 'from' and 'to' airports."
-                        }
-                    ]
-                }
-            })
-        )
-    })
-
-    it(`error by sending the non-existing IATA code of the destination airport`, () => {
-        calculateDistanceBetweenTwoAirports(endpoints.distance, departureId, 'led').should(
-            spok({
-                status: 422,
-                body: {
-                    "errors": [
-                        {
-                            "status": "422",
-                            "title": "Unable to process request",
-                            "detail": "Please enter valid 'from' and 'to' airports."
-                        }
-                    ]
-                }
-            })
-        )     
-    })
-
-    it(`error by sending the non-existing IATA code of both airports`, () => {
-        calculateDistanceBetweenTwoAirports(endpoints.distance, 'kid', 'led').should(
-            spok({
-                status: 422,
-                body: {
-                    "errors": [
-                        {
-                            "status": "422",
-                            "title": "Unable to process request",
-                            "detail": "Please enter valid 'from' and 'to' airports."
-                        }
-                    ]
-                }
-            })
-        )     
-    })
-
-    it('verifies schema for request with the non-existing IATA code of the departure airport', () => {
-        calculateDistanceBetweenTwoAirports(endpoints.distance, 'xxx', destinationId)
-            .validateSchema(status_422)
-    })
-
-    it('verifies schema for request with the non-existing IATA code of the destination airport', () => {
-        calculateDistanceBetweenTwoAirports(endpoints.distance, departureId, 'xxx')
-            .validateSchema(status_422)
-    })
-
-    it('verifies schema for request with the non-existing IATA code of both airports', () => {
-        calculateDistanceBetweenTwoAirports(endpoints.distance, 'xxx', 'yyy')
-            .validateSchema(status_422)
-    })
-
 })
--- cypress/e2e/API/getAirportById.cy.js
+++ cypress/e2e/API/getAirportById.cy.js
@@ -16,79 +16,83 @@ const { status_404 } = schemas.getAirportById
 const invalidTestData = dataGenerator.invalidIATACode()
 const validTestData = dataGenerator.validIATACode()
 
-let totalPages
+describe('get the airport by ID', () => {
+    let totalPages
 
-before(() => {
-    retrieveTotalPages(endpoints.airports).then((number) => totalPages = number)
-})
-
-describe('200 status code', () => {
-
-    it('returns the airport by the faker generated IATA code', () => {
-        fetchAirportById(endpoints.airports, validTestData.iataCode).then(response => {
-            cy.log(response.body)
-            expect(response.status, 'status code').to.equal(200)
-            expect(response.body.data.attributes.name, 'airport name').to.equal(validTestData.name)
-            expect(response.body.data.id, 'airport id').to.equal(validTestData.iataCode)
-        })
-    })
-    
-    it('returns the airport specified by IATA code and checks its name', () => {
-        let randomPage = Cypress._.random(1, totalPages)
-        let randomAirportNumber = Cypress._.random(0, airports.pagination.defaultLimit)
-        cy.log(randomPage, randomAirportNumber)
-        let airportsId, airportsName
-        //get the airport from random page and with random sequence number
-        //and retrieve airportsId and airportsName
-        fetchAirportsByPage(endpoints.airports, randomPage).then(response => {
-            airportsId = response.body.data[randomAirportNumber].id
-            airportsName = response.body.data[randomAirportNumber].attributes.name
-            return airportsId
-        }).then(airportsId => {
-        //get airport by retrieved airportsId
-            return fetchAirportById(endpoints.airports, airportsId)
-        }).then(response => {
-        //check if name of the airport in the body meets retrieved airportsName
-            expect(response.status, 'code status').to.equal(200)
-            expect(response.body.data.attributes.name).to.equal(airportsName)
-            expect(response.body.data.id).to.equal(airportsId)
-        })
+    before(() => {
+        retrieveTotalPages(endpoints.airports).then((number) => totalPages = number)
     })
 
-    it('verifies schema for request with valid IATA code', () => {
-        fetchAirportById(endpoints.airports, validTestData.iataCode)
-            .validateSchema(status_200)
+    context('200 status code', () => {
+
+        it('returns the airport by the faker generated IATA code', () => {
+            fetchAirportById(endpoints.airports, validTestData.iataCode).then(response => {
+                cy.log(response.body)
+                expect(response.status, 'status code').to.equal(200)
+                expect(response.body.data.attributes.name, 'airport name').to.equal(validTestData.name)
+                expect(response.body.data.id, 'airport id').to.equal(validTestData.iataCode)
+            })
+        })
+        
+        it('returns the airport specified by IATA code and checks its name', () => {
+            let randomPage = Cypress._.random(1, totalPages)
+            let randomAirportNumber = Cypress._.random(0, airports.pagination.defaultLimit)
+            cy.log(randomPage, randomAirportNumber)
+            let airportsId, airportsName
+            //get the airport from random page and with random sequence number
+            //and retrieve airportsId and airportsName
+            fetchAirportsByPage(endpoints.airports, randomPage).then(response => {
+                airportsId = response.body.data[randomAirportNumber].id
+                airportsName = response.body.data[randomAirportNumber].attributes.name
+                return airportsId
+            }).then(airportsId => {
+            //get airport by retrieved airportsId
+                return fetchAirportById(endpoints.airports, airportsId)
+            }).then(response => {
+            //check if name of the airport in the body meets retrieved airportsName
+                expect(response.status, 'code status').to.equal(200)
+                expect(response.body.data.attributes.name).to.equal(airportsName)
+                expect(response.body.data.id).to.equal(airportsId)
+            })
+        })
+    
+        it('verifies schema for request with valid IATA code', () => {
+            fetchAirportById(endpoints.airports, validTestData.iataCode)
+                .validateSchema(status_200)
+        })
+        
     })
     
+    context('404 status code', () => {
+    
+        for (let invalidId in invalidTestData) {
+            it(`error by sending the non-existing IATA code: ${invalidId}`, () => {
+                fetchAirportById(endpoints.airports, invalidTestData[invalidId]).should(
+                    spok({
+                        status: 404,
+                        body: {
+                            "errors": [
+                                {
+                                    "status": "404",
+                                    "title": "Not Found",
+                                    "detail": "The page you requested could not be found"
+                                }
+                            ]
+                        }
+                    })
+                )
+            })
+        }
+    
+        for (let invalidId in invalidTestData) {
+            it(`verifies schema for request with non-existing IATA code: ${invalidId}`, () => {
+                fetchAirportById(endpoints.airports, invalidTestData[invalidId])
+                    .validateSchema(status_404)
+            })
+        }
+         
+    })
+
 })
 
-describe('404 status code', () => {
 
-    for (let invalidId in invalidTestData) {
-        it(`error by sending the non-existing IATA code: ${invalidId}`, () => {
-            fetchAirportById(endpoints.airports, invalidTestData[invalidId]).should(
-                spok({
-                    status: 404,
-                    body: {
-                        "errors": [
-                            {
-                                "status": "404",
-                                "title": "Not Found",
-                                "detail": "The page you requested could not be found"
-                            }
-                        ]
-                    }
-                })
-            )
-        })
-    }
-
-    for (let invalidId in invalidTestData) {
-        it(`verifies schema for request with non-existing IATA code: ${invalidId}`, () => {
-            fetchAirportById(endpoints.airports, invalidTestData[invalidId])
-                .validateSchema(status_404)
-        })
-    }
-    
-    
-})
