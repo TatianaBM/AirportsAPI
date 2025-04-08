aed9c281fc8ddbd541f9be8c2f2e8140404a0dc7
LubovLepekhina <113927670+LubovLepekhina@users.noreply.github.com>
2025-04-08 16:30:25 +0200

    Add cypress-mochawesome-reporter (#35)
    * add mochawesome-reporter
    
    * update config and gitignore
    
    * update config

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,23 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   watchForFileChanges: false,
+  video: false,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/results',   // Folder where the reports will be saved
+    // reportFilename: '"[status]_[datetime]-[name]-report"',
+    charts: true,                   // Show charts (e.g., pie charts) in the HTML report
+    embeddedScreenshots: true,      // Embed screenshots directly into the report
+    inlineAssets: true,            // Inline JS/CSS into HTML file (no external links)
+    saveAllAttempts: true,          // Save screenshots from all retry attempts, set to false to save only the last attempt
+    overwrite: true,                // Overwrite previous reports
+    videoOnFailOnly: true           // Add the videos to report only to tests with failures
+  },
   e2e: {
     baseUrl: 'https://airportgap.com/api',
+    retries: 1,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
 });
