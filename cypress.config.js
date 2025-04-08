254137a9b5c8490ee680bfa725e55237eaa062c2
LubovLepekhina <lubov.lepekhina@gmail.com>
2025-04-08 11:09:49 +0200

    update config and gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -4,15 +4,17 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   watchForFileChanges: false,
+  video: false,
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: 'cypress/results',
-    charts: true,
-    embeddedScreenshots: true,
-    inlineAssets: false,
-    saveAllAttempts: true,
-    overwrite: true,
-    videoOnFailOnly: true
+    reportDir: 'cypress/results',   // Folder where the reports will be saved
+    // reportFilename: '"[status]_[datetime]-[name]-report"',
+    charts: true,                   // Show charts (e.g., pie charts) in the HTML report
+    embeddedScreenshots: true,      // Embed screenshots directly into the report
+    inlineAssets: false,            // Inline JS/CSS into HTML file (no external links)
+    saveAllAttempts: true,          // Save screenshots from all retry attempts, set to false to save only the last attempt
+    overwrite: true,                // Overwrite previous reports
+    videoOnFailOnly: true           // Add the videos to report only to tests with failures
   },
   e2e: {
     baseUrl: 'https://airportgap.com/api',
