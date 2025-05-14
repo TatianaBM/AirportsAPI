7a7a039e09b9cc74b42f4ab87f0db5baf21e6988
Tatiana BM <burykina.tatiana@gmail.com>
2025-05-14 15:10:08 +0200

    add header test calc distance

--- cypress/e2e/API/calculateDistanceLL.cy.js
+++ cypress/e2e/API/calculateDistanceLL.cy.js
@@ -6,9 +6,11 @@ import { endpoints } from "../../support/endpoints"
 import { dataGenerator } from "../../support/testData"
 import schemas from '../../fixtures/schemas.json'
 import errors from '../../fixtures/errors.json'
+import airports from '../../fixtures/airports.json'
 
 const { status_200, status_422 } = schemas.calculateDistance
 const { status_422_error } = errors.calculateDistance
+const { request, response } = airports.headers['content-type']
 const departureId = dataGenerator.validIATACode().iataCode
 const destinationId = dataGenerator.validIATACode().iataCode
 
@@ -26,6 +28,24 @@ describe('calculates the distance between two airports', () => {
                     expect(response.body.data.attributes.nautical_miles).to.be.at.least(0)
                 })
         })
+
+        it('checks headers: content type', () => {
+            calculateDistanceBetweenTwoAirports(
+                endpoints.distance,
+                departureId,
+                destinationId,
+            ).should(
+                spok({
+                    status: 200,
+                    headers: {
+                        'content-type': response.json,
+                    },
+                    requestHeaders: {
+                        'content-type': request.json,
+                    },
+                }),
+            )
+        })
     
         it('verifies schema for request with two valid IATA codes', () => {
             calculateDistanceBetweenTwoAirports(endpoints.distance, departureId, destinationId)
@@ -39,6 +59,12 @@ describe('calculates the distance between two airports', () => {
             calculateDistanceBetweenTwoAirports(endpoints.distance, 'led', destinationId).should(
                 spok({
                     status: 422,
+                    headers: {
+                        'content-type': response.json,
+                    },
+                    requestHeaders: {
+                        'content-type': request.json,
+                    },
                     body: status_422_error
                 })
             )
@@ -48,6 +74,12 @@ describe('calculates the distance between two airports', () => {
             calculateDistanceBetweenTwoAirports(endpoints.distance, departureId, 'led').should(
                 spok({
                     status: 422,
+                    headers: {
+                        'content-type': response.json,
+                    },
+                    requestHeaders: {
+                        'content-type': request.json,
+                    },
                     body: status_422_error
                 })
             )     
@@ -57,6 +89,12 @@ describe('calculates the distance between two airports', () => {
             calculateDistanceBetweenTwoAirports(endpoints.distance, 'kid', 'led').should(
                 spok({
                     status: 422,
+                    headers: {
+                        'content-type': response.json,
+                    },
+                    requestHeaders: {
+                        'content-type': request.json,
+                    },
                     body: status_422_error
                 })
             )     
