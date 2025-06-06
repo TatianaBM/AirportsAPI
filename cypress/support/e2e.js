9c2d25e88d1efc313df451c38baec596132c0423
LubovLepekhina <lubov.lepekhina@gmail.com>
2025-06-06 13:24:08 +0200

    revert e2e.js, delete extra code from merge-report job

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -31,9 +31,6 @@ before(() => {
 const titleToFileName = (title) => title.replace(/[:\/]/g, '')
 
 Cypress.on('test:after:run', (test, runnable) => {
-    let contextPath = (file) =>
-        `screenshots/${Cypress.spec.name}/${file}`
-
     if (test.state === 'failed') {
         let parent = runnable.parent
         let filename = ''
@@ -42,9 +39,8 @@ Cypress.on('test:after:run', (test, runnable) => {
             parent = parent.parent
         }
         filename += `${titleToFileName(test.title)} (failed).png`
-        addContext({ test }, contextPath('screenshot', filename))
+        addContext({ test }, `../screenshots/${Cypress.spec.name}/${filename}`)
     }
-
-    const videoName = `${Cypress.spec.name}.mp4`
-    addContext({ test }, `videos/${videoName}`)
+    // always add the video
+    addContext({ test }, `../videos/${Cypress.spec.name}.mp4`)
 })
